4033af1e4a17eb52801c62990905811987467d7d
Arthur Lapz <67346305+rlapz@users.noreply.github.com>
2021-02-21 23:00:22 +0700

    Update seriallib.h

--- seriallib.h
+++ seriallib.h
@@ -14,6 +14,6 @@ int serial_open(const char* port, unsigned int baud,
 		int parity, int stopbits, int bytesize, int vmin, int vtime);
 int serial_write(int* portdesc, unsigned char* data, size_t len);
 int serial_read(int* portdesc, unsigned char* buffer, size_t len);
-void serial_close(int*);
+void serial_close(int* portdesc);
 
 #endif
